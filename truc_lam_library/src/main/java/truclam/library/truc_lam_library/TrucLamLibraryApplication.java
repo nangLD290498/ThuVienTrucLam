579984ecaa2a514eba2ede5ff561e7023320ee1c
Hiep Tran <tthiep2@cmcglobal.vn>
2023-07-13 17:02:08 +0700

    project initial

---
+++ truc_lam_library/src/main/java/truclam/library/truc_lam_library/TrucLamLibraryApplication.java
@@ -0,0 +1,13 @@
+package truclam.library.truc_lam_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucLamLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrucLamLibraryApplication.class, args);
+    }
+
+}
---
+++ truc_lam_library/src/test/java/truclam/library/truc_lam_library/TrucLamLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package truclam.library.truc_lam_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucLamLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
