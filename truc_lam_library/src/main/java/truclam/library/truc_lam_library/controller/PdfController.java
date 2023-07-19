d52fb2ac3bef6c52713da9865e74bd1f49c0b7e2
Hiep Tran <tthiep2@cmcglobal.vn>
2023-07-19 17:55:21 +0700

    test deploy

--- truc_lam_library/src/main/java/truclam/library/truc_lam_library/controller/PdfController.java
+++ truc_lam_library/src/main/java/truclam/library/truc_lam_library/controller/PdfController.java
@@ -32,7 +32,7 @@ public class PdfController {
 
     @PostMapping("/pdf/saveBookInfor")
     public ResponseObject saveBookInfor(@RequestBody Book book) {
-        logger.info("exec saveBookInfor api");
+        logger.info("execute saveBookInfor api");
         return pdfService.saveBookInfor( book);
     }
 
