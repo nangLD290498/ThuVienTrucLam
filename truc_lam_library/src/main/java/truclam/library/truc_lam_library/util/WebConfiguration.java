d2c23e647488efe26661920f0c7d617c6c8133fc
NangLD290498 <nangld290498@gmail.com>
2023-07-22 10:53:46 +0700

    nld update from server

---
+++ truc_lam_library/src/main/java/truclam/library/truc_lam_library/util/WebConfiguration.java
@@ -0,0 +1,14 @@
+package truclam.library.truc_lam_library.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
