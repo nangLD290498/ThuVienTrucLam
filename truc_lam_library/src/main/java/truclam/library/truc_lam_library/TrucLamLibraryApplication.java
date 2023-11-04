6939baf5ad7373233c1a302819d72c238972b589
NangLD290498 <nangld290498@gmail.com>
2023-11-04 23:07:02 +0700

    nld test

--- truc_lam_library/src/main/java/truclam/library/truc_lam_library/TrucLamLibraryApplication.java
+++ truc_lam_library/src/main/java/truclam/library/truc_lam_library/TrucLamLibraryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrucLamLibraryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TrucLamLibraryApplication.class, args);
     }
